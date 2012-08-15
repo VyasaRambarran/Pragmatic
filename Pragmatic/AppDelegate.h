5bd76013a6719d1b837617ec223d613d35bfdb1e
Vyasa Rambarran <Vyasa@Vyasas-MacBook-Pro.local>
2012-08-15 17:33:55 +0200

    Initial Commit

---
+++ Pragmatic/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Pragmatic
+//
+//  Created by Vyasa Rambarran on 2012/08/15.
+//  Copyright (c) 2012 Vyasa Rambarran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pragmatic/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Pragmatic
+//
+//  Created by Vyasa Rambarran on 2012/08/15.
+//  Copyright (c) 2012 Vyasa Rambarran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
